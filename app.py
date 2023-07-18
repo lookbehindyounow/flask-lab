d36fd533368e3ca8310206d60f3520e0dae2158d
Kieron Harrower <somebody.else.2017@gmail.com>
2023-07-18 14:41:54 +0100

    copied eveything over from flask mvc lesson

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from controllers.tasks_controller import tasks_blueprint
+
+app = Flask(__name__)
+app.register_blueprint(tasks_blueprint)
+
+@app.route('/')
+def index():
+    return "hhhhh"
