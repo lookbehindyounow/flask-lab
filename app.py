7bca6a20549435eec27fee611c500156e19f482f
Kieron Harrower <somebody.else.2017@gmail.com>
2023-07-18 15:26:24 +0100

    made individual order pages accessed by home/order/<order_number>. links not made yet

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from controllers.orders_controller import orders_blueprint
 
-app = Flask(__name__)
+app=Flask(__name__)
 app.register_blueprint(orders_blueprint)
 
 @app.route('/')
--- controllers/orders_controller.py
+++ controllers/orders_controller.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint,render_template
 from models.order_list import orders
 
 orders_blueprint=Blueprint("Orders",__name__)
@@ -6,3 +6,7 @@ orders_blueprint=Blueprint("Orders",__name__)
 @orders_blueprint.route("/orders")
 def index():
     return render_template("index.html",title="Order list",orders_for_jinja=orders)
+
+@orders_blueprint.route("/orders/<order_number>")
+def order(order_number):
+    return render_template("order.html",title=f"Order number {order_number}",order=orders[int(order_number)])
